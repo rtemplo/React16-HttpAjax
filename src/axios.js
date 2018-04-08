16b09074fef6ff32a9bfbef9329792bde9c233e4
Ray Templo <rtemplo@gmail.com>
2018-04-07 21:26:16 -0400

    integrated additional feartures to axios instance for demo

--- src/axios.js
+++ src/axios.js
@@ -5,5 +5,28 @@ const instance = axios.create({
 });
 
 instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN 2 - FROM INSTANCE';
+// instance.defaults.headers.post['Content-Type'] = 'application/json';
+
+// //to add interceptors
+// instance.interceptors.request.use(request => {
+//   console.log('From requestInterceptor: ' + request);
+//   //you can edit the request here before returning it
+//   return request;
+// }, error => {
+//   console.log('From requestInterceptor (error): ' + error);
+//   return Promise.reject(error);
+// });
+
+// instance.defaults.responseType.use(response => {
+//   console.log('From responseInterceptor: ' + response);
+//   return response;
+// }, error => {
+//   console.log('From responseInterceptor (error): ' + error);
+//   return Promise.reject(error);
+// });
+
+// //to remove interceptors
+// axios.interceptors.request.eject(requestInterceptor);
+// axios.interceptors.response.eject(responseInterceptor);
 
 export default instance;
